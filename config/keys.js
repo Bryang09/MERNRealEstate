8a5c0a75c9d542a02529ee862debb0737c032070
Bryan Gonzalez <bryan.gonzalez1020.bg@gmail.com>
2019-08-25 10:01:58 -0500

    Server Running

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://user:user123@ds117623.mlab.com:17623/real_estate"
+};
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// BODY PARSER MIDDLEWARE
+app.use(bodyParser.json());
+
+// DB CONFIG
+const db = require("./config/keys").mongoURI;
+
+// CONNECT TO MONGO
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, console.log(`Connected to port ${port}`));
